85202c0548c85a20ccd4c71de9742a8009db0206
Andrei Portales <andreifan123@gmail.com>
2022-08-31 00:16:51 -0600

    Initial project

---
+++ src/main/java/Client.java
@@ -0,0 +1,24 @@
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+
+public class Client {
+    private final String SERVER_HOST = "alumchat.fun";
+    private final int SERVER_PORT = 5222;
+    private XMPPConnection connection;
+
+    public Client() {
+        ConnectionConfiguration config = new ConnectionConfiguration(SERVER_HOST, SERVER_PORT);
+        config.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
+        config.setDebuggerEnabled(false);
+        config.setSendPresence(true);
+
+        connection = new XMPPConnection(config);
+
+        try {
+            connection.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
